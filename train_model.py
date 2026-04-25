70001a24663f13a69df25f07ef480d12a8731cfc
Maria Madalena <mariamada496@gmail.com>
2026-04-25 23:50:12 +0000

    carrega e preprocessa dataset MNIST

--- train_model.py
+++ train_model.py
@@ -3,3 +3,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 #insira seu código aqui
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train = x_train / 255.0
+x_test = x_test / 255.0

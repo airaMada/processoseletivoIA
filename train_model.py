f7b542052639a5fb1bf22a37aba36977b5fb2339
Maria Madalena <mariamada496@gmail.com>
2026-04-26 00:20:53 +0000

    implementa arquitetura do modelo CNN

--- train_model.py
+++ train_model.py
@@ -1,9 +1,23 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras import layers, models
 
 #insira seu código aqui
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 x_train = x_train / 255.0
 x_test = x_test / 255.0
+
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64, (3,3), activation= 'relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Flatten(),
+    layers.Dense(64, activation= 'relu'),
+    layers.Dense(10, activation= 'relu'),
+
+])
